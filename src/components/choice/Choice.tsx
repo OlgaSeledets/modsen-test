6438ed8f2ef2a239664e63bd6ccdf8500e7773e1
Olga Seledets <olga.seledets@outlook.com>
2023-06-19 23:08:40 +0300

    WIP: add Choice component

---
+++ src/components/choice/Choice.tsx
@@ -0,0 +1,24 @@
+import './Choice.css'
+
+type Option<T> = {
+  caption: string,
+  value: T
+}
+
+type ChoiceProps<T> = {
+  onChooseOption: (e: React.ChangeEvent<HTMLSelectElement>) => void,
+  options: Array<Option<T>>
+}
+
+function Choice<T extends string>(props: ChoiceProps<T>): JSX.Element {
+  return (
+    <select
+      className="subheader__select select-common"
+      onChange={props.onChooseOption}
+    >
+      {props.options.map(option => <option value={option.value}>{option.caption}</option>)}
+    </select>
+  )
+}
+
+export default Choice
